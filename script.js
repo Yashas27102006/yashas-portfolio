a36e8f14ce6a04e25b6cd78b4e458c44517be0ac
Yashas.L <Yashuyashas963@gmail.com>
2026-09-19 22:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+// EmailJS
+emailjs.init({
+    publicKey: "4UuJ22QkwVbB_20ov"
+});
+
+// Mobile menu
 const menuIcon = document.querySelector(".menu-icon");
 const nav = document.querySelector("nav");
 
 menuIcon.addEventListener("click", () => {
     nav.classList.toggle("show");
+});
+
+// Contact form
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    emailjs.sendForm(
+        "service_mcvp67x",
+        "template_pwda13t",
+        this
+    ).then(() => {
+        alert("Message sent successfully!");
+        this.reset();
+    }).catch((error) => {
+        alert("Failed to send message. Please try again.");
+        console.error(error);
+    });
 });
